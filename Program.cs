0f880512473cf167cab934ae8cb16ec7283305b1
a1235712 <a1235712@gmail.com>
2022-05-03 17:20:18 +0300

    Добавлена программа Является ли числ степенью двойки

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Определите, является ли число степенью двойки:
+//N = 16 -> "Является степень двойки"
+//N = 12 -> “Не является степенью двойки”
+
+bool IsPowerOfTwo(int number)
+{
+    for (int x = 1; x <= number; x *= 2)
+    {
+        if (x == number) return true;
+    }
+    return false;
+}
+
+Console.WriteLine("Введите число N: ");
+
+string strN = Console.ReadLine();
+int N = int.Parse(strN);
+
+IsPowerOfTwo(N);
+
+if (IsPowerOfTwo(N) == true) Console.Write($"{N} является степенью двойки");
+else Console.WriteLine($"{N} не является степенью двойки");
+
